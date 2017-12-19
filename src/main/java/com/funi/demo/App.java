aedadf8192318bbb15fd86fe38d26236a16c1a0a
niuzhihuan <zhihuan.niu@funi.com>
2017-12-19 15:44:10 +0800

    update

--- src/main/java/com/funi/demo/App.java
+++ src/main/java/com/funi/demo/App.java
@@ -2,19 +2,30 @@ package com.funi.demo;
 
 import org.mybatis.generator.exception.XMLParserException;
 
+import java.awt.EventQueue;
 import java.io.IOException;
 
 /**
  * 启动窗口
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, XMLParserException {
-        CodeGeneratorForm from=new CodeGeneratorForm();
-        from.pack();
-        from.setSize(1280, 600);
-        from.setLocation(10,10);
-        from.setVisible(true);
-    }
+public class App {
+	public static void main(String[] args){
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				CodeGeneratorForm from;
+				try {
+					from = new CodeGeneratorForm();
+					from.pack();
+					from.setSize(1280, 600);
+					from.setLocation(10, 10);
+					from.setVisible(true);
+				} catch (IOException | XMLParserException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
 }
